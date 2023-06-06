58d2a73eb465aa68c32f2a869241834f5283eb1b
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2023-06-05 22:34:08 -0300

    generatePassword update

--- ex004-PasswordGenerator/src/App.jsx
+++ ex004-PasswordGenerator/src/App.jsx
@@ -1,21 +1,34 @@
 import Button from "./components/Button"
 import Card from "./components/Card"
+import Footer from "./components/Footer"
 import Input from "./components/Input"
+import LinkButton from "./components/LinkButton"
 import Title from "./components/Title"
 
 
 
 export default () => {
     return (
-        <div className="app" style={{
-            display: "grid",
-            placeContent: "center",
-            textAlign: "center",
-            gap: "5rem",
-            minHeight: "100vh"
-        }}>
-            <Title text={"Gerador de Senhas"}/>
-            <Card />
-        </div>
+        <>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                minHeight: "100vh",
+            }}>
+                <section className="main-content" style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flex: "1",
+                    gap: "5rem"
+
+                }}>
+                    <Title text={"Gerador de Senhas"} />
+                    <Card />
+                </section>
+                <Footer />
+            </div>
+        </>
     )
 }
