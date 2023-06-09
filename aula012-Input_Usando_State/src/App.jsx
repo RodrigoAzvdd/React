dc8383c7ec549c23dfec4734b003bfdba56d0904
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2023-06-09 18:32:34 -0300

    commit 012 changes

--- aula012-Input_Usando_State/src/App.jsx
+++ aula012-Input_Usando_State/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [password, setPassword] = useState("")
+    const [copyText, setCopyText] = useState("Copiar")
+    const [passwordSize, setPasswordSize] = useState(10)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function generate() {
+        const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
+        let newPassword = ""
+        for (let i = 0; i < passwordSize; i++) {
+            const position = Math.floor(Math.random() * characters.length)
+            newPassword += characters[position]
+        }
+        setPassword(newPassword)
+        setCopyText("Copiar")
+    }
+
+    function copyToClipboard() {
+        window.navigator.clipboard.writeText(password)
+        setCopyText("Copiado!")
+    }
+
+    return (
+        <div>
+            <h1>Gerador de senhas</h1>
+            <div>
+                <label htmlFor="passwordSize">Tamanho: </label>
+                <input
+                    type="number"
+                    id="passwordSize"
+                    min={1}
+                    value={passwordSize}
+                    onChange={(ev) => {
+                        console.log(ev.target)
+                        console.log(ev.target.value)
+
+                        setPasswordSize(ev.target.value)
+                    }} />
+            </div>
+            <button onClick={generate}>Gerar senha de {passwordSize} characters!</button>
+            <button onClick={copyToClipboard}>{copyText}</button>
+            <div>
+                <p>{password}</p>
+            </div>
+        </div>
+    )
 }
 
 export default App
