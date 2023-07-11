3d3569a9857bbd0d3cae89db2ddf04da8b8f9f74
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2023-07-11 18:28:28 -0300

    useEffect

---
+++ aula017-UseEffect/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function Counter() {
+
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    alert(`Iniciando Busca...`)
+
+    return () => {
+      alert("Busca Cancelada!")
+    }
+  }, [])
+
+  return (
+    <div>
+      <p>Busca Iniciada...</p>
+    </div>
+  )
+}
+
+function App() {
+
+  const [show, setShow] = useState(false)
+
+  return (
+    <div className='div'>
+      <h1>UseEffect</h1>
+      <label htmlFor="check">Iniciar Buscar</label>
+      <input type="checkbox" id="check" value={show} onChange={() => {
+        setShow(state => !state)
+      }} />
+      {
+        show ? <Counter /> : null
+      }
+    </div>
+  )
+}
+
+export default App
